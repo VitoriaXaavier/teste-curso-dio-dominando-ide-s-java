872938096b3eec30d906102e88b2ebd485115da9
Vitória Alves Xavier Dias <vitoria_xavierd@outlook.com>
2022-07-15 17:56:03 -0300

    Add files via upload

---
+++ primeiroProjeto.java
@@ -0,0 +1,13 @@
+package com.dio;
+
+import com.dio.model.Gate;
+
+public class primeiroProjeto {
+
+	public static void main(String[] args) 
+	{
+		Gate gate = new Gate(null, null, null);
+		System.out.println(gato);
+
+	}
+}
